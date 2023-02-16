06b7c8bf8ee0eefb2bee9a4eefbd32bce453f4a0
Maksym Solomko <max.exc7@gmail.com>
2023-02-16 21:19:22 +0200

    update

--- App.js
+++ App.js
@@ -1,90 +1,94 @@
-import React, { useState } from "react";
-import {
-  StyleSheet,
-  View,
-  TextInput,
-  TouchableWithoutFeedback,
-  Keyboard,
-  KeyboardAvoidingView,
-  Platform,
-  Alert,
-  Button,
-} from "react-native";
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+import { LoginScreen } from './components/auth/LoginScreen';
+import { RegistrationScreen } from './components/auth/RegistrationScreen';
 
 export default function App() {
-  const [name, setName] = useState("");
-  const [password, setPassword] = useState("");
-
-  const nameHandler = (text) => setName(text);
-  const passwordHandler = (text) => setPassword(text);
-
-  const onLogin = () => {
-    Alert.alert("Credentials", `${name} + ${password}`);
-  };
-
   return (
-    <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
-      <View style={styles.container}>
-        <KeyboardAvoidingView
-          behavior={Platform.OS == "ios" ? "padding" : "height"}
-        >
-          <TextInput
-            value={name}
-            onChangeText={nameHandler}
-            placeholder="Username"
-            style={styles.input}
-          />
-          <TextInput
-            value={password}
-            onChangeText={passwordHandler}
-            placeholder="Password"
-            secureTextEntry={true}
-            style={styles.input}
-          />
-          <Button title={"Login"} style={styles.input} onPress={onLogin} />
-        </KeyboardAvoidingView>
-      </View>
-    </TouchableWithoutFeedback>
+    <View style={styles.container}>
+      <RegistrationScreen />
+      <LoginScreen />     
+      <StatusBar style="auto" />
+    </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: "center",
-    justifyContent: "center",
-    backgroundColor: "#ecf0f1",
-  },
-  input: {
-    width: 200,
-    height: 44,
-    padding: 10,
-    borderWidth: 1,
-    borderColor: "black",
-    marginBottom: 10,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
   },
 });
 
 
 
-// import { StatusBar } from 'expo-status-bar';
-// import { StyleSheet, Text, View } from 'react-native';
+
+
+// import React, { useState } from "react";
+// import {
+//   StyleSheet,
+//   View,
+//   TextInput,
+//   TouchableWithoutFeedback,
+//   Keyboard,
+//   KeyboardAvoidingView,
+//   Platform,
+//   Alert,
+//   Button,
+// } from "react-native";
 
 // export default function App() {
+//   const [name, setName] = useState("");
+//   const [password, setPassword] = useState("");
+
+//   const nameHandler = (text) => setName(text);
+//   const passwordHandler = (text) => setPassword(text);
+
+//   const onLogin = () => {
+//     Alert.alert("Credentials", `${name} + ${password}`);
+//   };
+
 //   return (
-//     <View style={styles.container}>
-//       <Text>Open up App.js to start working on your app! test</Text>
-//       <Text>Why does not working? HELLO </Text>
-//       <StatusBar style="auto" />
-//     </View>
+//     <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+//       <View style={styles.container}>
+//         <KeyboardAvoidingView
+//           behavior={Platform.OS == "ios" ? "padding" : "height"}
+//         >
+//           <TextInput
+//             value={name}
+//             onChangeText={nameHandler}
+//             placeholder="Username"
+//             style={styles.input}
+//           />
+//           <TextInput
+//             value={password}
+//             onChangeText={passwordHandler}
+//             placeholder="Password"
+//             secureTextEntry={true}
+//             style={styles.input}
+//           />
+//           <Button title={"Login"} style={styles.input} onPress={onLogin} />
+//         </KeyboardAvoidingView>
+//       </View>
+//     </TouchableWithoutFeedback>
 //   );
 // }
 
 // const styles = StyleSheet.create({
 //   container: {
 //     flex: 1,
-//     backgroundColor: '#fff',
-//     alignItems: 'center',
-//     justifyContent: 'center',
+//     alignItems: "center",
+//     justifyContent: "center",
+//     backgroundColor: "#ecf0f1",
+//   },
+//   input: {
+//     width: 200,
+//     height: 44,
+//     padding: 10,
+//     borderWidth: 1,
+//     borderColor: "black",
+//     marginBottom: 10,
 //   },
 // });
